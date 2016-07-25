8059b3333e9343c51ebc87df9cdb5908704395f1
mammix2 <mammix2@hotmail.com>
2016-07-25 18:05:04 +0100

    update the rest of the protocol versions - hard fork

--- src/version.h
+++ src/version.h
@@ -39,15 +39,15 @@ static const int INIT_PROTO_VERSION = 209;
 static const int MIN_PEER_PROTO_VERSION = 90000;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 60013; 
+static const int MIN_POOL_PEER_PROTO_VERSION = 90000;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 60013;
+static const int MIN_INSTANTX_PROTO_VERSION = 90000;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 60013;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 60013;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 90000;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 90000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
