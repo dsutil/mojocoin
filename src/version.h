388d62a146b793cb7472b416c6a84f173c828559
Christian Knöpke <apfelbaum@email.de>
2016-11-05 14:12:40 +0100

    Changes in version

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90001;
+static const int PROTOCOL_VERSION = 90002;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -39,15 +39,15 @@ static const int INIT_PROTO_VERSION = 209;
 static const int MIN_PEER_PROTO_VERSION = 90001;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 90000;
+static const int MIN_POOL_PEER_PROTO_VERSION = 90001;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 90000;
+static const int MIN_INSTANTX_PROTO_VERSION = 90001;
 
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 90000;
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 90000;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 90001;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 90001;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
